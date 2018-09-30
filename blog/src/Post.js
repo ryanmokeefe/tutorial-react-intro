61bee14702e4d915e7ae575a61043e66bdb90182
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 22:04:35 -0400

    backup

--- blog/src/Post.js
+++ blog/src/Post.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react'
 import Comment from './Comment.js'
 
 class Post extends Component {
+
     render () {
+
+        let comments = this.props.comments.map((comment, index) => (
+            <Comment message={comment} key={index} />
+        ))
+    
         return (
         <div className="Post">
             <h1>{this.props.title}</h1>
@@ -10,9 +16,11 @@ class Post extends Component {
             <p>{this.props.body}</p>
             
             <h3>Comments:</h3>
-            <Comment message={this.props.comments[0]} />
+            {/* <Comment message={this.props.comments[0]} />
             <Comment message={this.props.comments[1]} />
-            <Comment message={this.props.comments[2]} />
+            <Comment message={this.props.comments[2]} /> */}
+            {comments}
+            <Comment message="hello world"/>
 
         </div>
     )}
